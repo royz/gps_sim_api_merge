78936de7ae2158f609f62857d74d6b76268b0f86
Rajorshi Roy <35338951+royz@users.noreply.github.com>
2020-08-22 02:28:46 +0530

    all loops are implemented. exceptions are handed.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import time
 from pprint import pprint
 import config
 import logging
@@ -10,8 +11,6 @@ from bs4 import BeautifulSoup
 
 
 class Navixy:
-    API_BASE = 'https://api.navixy.com/v2'
-
     def __init__(self):
         self.username = config.navixy_username
         self.password = config.navixy_password
@@ -29,7 +28,8 @@ class Navixy:
             'password': self.password
         }
         try:
-            response = requests.post(f'{self.API_BASE}/user/auth', headers=headers, json=data).json()
+            response = requests.post('https://api.navixy.com/v2/user/auth',
+                                     headers=headers, json=data).json()
             if response['success']:
                 self.user_hash = response['hash']
                 logger.info('logged into navixy account')
@@ -48,12 +48,26 @@ class Navixy:
             'hash': self.user_hash
         }
 
-        response = requests.post(f'{self.API_BASE}/tracker/list', headers=headers, json=data)
+        try:
+            response = requests.post('https://api.navixy.com/v2/tracker/list',
+                                     headers=headers, json=data)
+            try:
+                json_response = response.json()
+                if json_response['success']:
+                    return [{
+                        'sim_number': tracker['source']['phone'],
+                        'is_blocked': tracker['source']['blocked']
+                    } for tracker in json_response['list']]
+                else:
+                    logger.error('could not get trackers list from Navixy')
+                    return None
+            except Exception as e:
+                logger.error(get_error_str(e))
+                return None
 
-        with open('trackers.json', 'w', encoding='utf-8') as f:
-            json.dump(response.json(), f)
-
-        pprint(response.json())
+        except Exception as e:
+            logger.error(get_error_str(e))
+            return None
 
 
 class ThingsMobile:
@@ -62,8 +76,6 @@ class ThingsMobile:
     def __init__(self, username=None, token=None):
         self.username = username or config.things_mobile_username
         self.token = token or config.things_mobile_token
-        print(self.username)
-        print(self.token)
 
     def sim_status(self, sim_number: str) -> bool:
         try:
@@ -78,8 +90,9 @@ class ThingsMobile:
             soup = BeautifulSoup(data, 'lxml-xml')
             return soup.find('status').text == 'active'
         except Exception as e:
-            logger.error(e)
+            logger.error(get_error_str(e))
             logger.error(f'failed to retrieve information for {sim_number}')
+            return None
 
     def block_sim(self, sim_number: str = '882360012289512') -> bool:
         try:
@@ -114,25 +127,93 @@ class ThingsMobile:
             logger.error(f'failed to unblock sim: {sim_number}')
 
 
+def get_error_str(err: Exception):
+    return ' '.join(str(err).split())
+
+
 if __name__ == '__main__':
+    # configure the logger
     logging.basicConfig(
         level=logging.DEBUG,
         format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
         handlers=(
             RotatingFileHandler(
-                filename='flex.log',
+                filename='api-merge.log',
                 maxBytes=(1024 ** 3) / 2,
                 backupCount=1,
             ),
             logging.StreamHandler(sys.stdout)
         )
     )
-    # logging.getLogger(aiohttp.__name__).setLevel(logging.ERROR)
     logger = logging.getLogger()
 
-    # navixy = Navixy()
-    # navixy.get_tracker_list()
-
+    navixy = Navixy()
     things_mobile = ThingsMobile()
-    status = things_mobile.unblock_sim('882360012774864')
-    print(status)
+
+    while True:
+        try:
+            trackers_fetch_time = time.time()
+            # get navixy trackers list
+
+            trackers_list = navixy.get_tracker_list()
+            if not trackers_list:
+                # sleep for 30 minutes and try again
+                time.sleep(1800)
+                continue
+            else:
+                logger.info(f'{len(trackers_list)} trackers found from Navixy')
+
+            # for each tracker check status and block or unblock
+            # the sim depending on the Navixy blocked status.
+            # also wait 5 minutes between each sim/tracker request
+            for tracker in trackers_list:
+                tracker_loop_start = time.time()
+
+                # check status on things mobile for this sim
+                things_mobile_status = things_mobile.sim_status(tracker['sim_number'])
+
+                if things_mobile_status is None:
+                    # sleep for 2 minutes and continue to next tracker
+                    time.sleep(120)
+                    continue
+
+                elif things_mobile_status is True:
+                    if tracker['is_blocked']:
+                        # when sim is active but gps is blocked: block sim
+                        logger.info(f"[{tracker['sim_number']}] status:  sim is active but gps is blocked")
+                        status = things_mobile.block_sim(tracker['sim_number'])
+                        if status:
+                            logger.info(f"[{tracker['sim_number']}] was successfully blocked")
+                        else:
+                            logger.error(f"could not block [{tracker['sim_number']}]")
+                    else:
+                        # when sim and gps both active: do nothing
+                        logger.info(f"[{tracker['sim_number']}] status:  sim and gps both active")
+                else:
+                    if tracker['is_blocked']:
+                        # when sim and gps both blocked: do nothing
+                        logger.info(f"[{tracker['sim_number']}] status:  sim and gps both blocked")
+                    else:
+                        # when sim is blocked but gps is active: unblock the sim
+                        logger.info(f"[{tracker['sim_number']}] status:  sim is blocked but gps is active")
+                        status = things_mobile.unblock_sim(tracker['sim_number'])
+                        if status:
+                            logger.info(f"[{tracker['sim_number']}] was successfully unblocked")
+                        else:
+                            logger.error(f"could not unblock [{tracker['sim_number']}]")
+
+                # sleep for some time so that the inner loop takes 5 minutes in total
+                time_taken = time.time() - tracker_loop_start
+                if time_taken < config.inner_loop_time:
+                    time_to_sleep = config.inner_loop_time - time_taken
+                    logger.info(f'sleeping for {round(time_to_sleep)} sec')
+                    time.sleep(time_to_sleep)
+
+            # sleep for some time so that the outer loop takes 2 hours in total
+            time_taken = time.time() - trackers_fetch_time
+            if time_taken < config.outer_loop_time:
+                time_to_sleep = config.outer_loop_time - time_taken
+                logger.info(f'sleeping for {round(time_to_sleep)} sec')
+                time.sleep(time_to_sleep)
+        except Exception as e:
+            logger.critical(get_error_str(e))
